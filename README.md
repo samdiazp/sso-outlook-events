f1c9c1c4f49676c73372be2d074919fe9602ad68
samueldiazmelt <110833932+samueldiazmelt@users.noreply.github.com>
2023-03-09 16:42:45 -0500

    sample

---
+++ README.md
@@ -0,0 +1,118 @@
+---
+title: "Use SSO with event-based activation in an Outlook add-in"
+page_type: sample
+urlFragment: outlook-add-in-sso-event-based-activation
+products:
+- office-add-ins
+- office-outlook
+- office
+languages:
+- javascript
+extensions:
+  contentType: samples
+  technologies:
+  - Add-ins
+  createdDate: 02/21/2023 10:00:00 AM
+description: "Use SSO with event-based activation in an Outlook add-in."
+---
+
+# Use SSO with event-based activation in an Outlook add-in
+
+**Applies to**: Outlook on Windows | Outlook on the web | [new Outlook on Mac](https://support.microsoft.com/office/6283be54-e74d-434e-babb-b70cefc77439)
+
+## Summary
+
+The sample shows how to use SSO to access a user's Microsoft Graph data from an event fired in an Outlook add-in. The sample illustrates the following concepts:
+
+- [Get a user access token using SSO](https://learn.microsoft.com/outlook/add-ins/authenticate-a-user-with-an-sso-token) when the [OnNewMessageCompose event](https://learn.microsoft.com/office/dev/add-ins/outlook/autolaunch) fires.
+- Implement a server REST API that uses the [Microsoft identity platform and OAuth 2.0 On-Behalf-Of flow](https://learn.microsoft.com/azure/active-directory/develop/v2-oauth2-on-behalf-of-flow) to exchange the user's access token for a new access token with permissions to the users profile on Microsoft Graph.
+- Use the [Microsoft Graph API](https://developer.microsoft.com/graph/docs/api-reference/v1.0/resources/onedrive) to get user profile data such as display name and job title.
+- Construct a signature in the mail item containing the user profile data.
+
+## Register the add-in with Microsoft identity platform
+
+Use the following values for the subsequent app registration steps.
+
+| Placeholder or section | Value          |
+|------------------------|----------------|
+| `<add-in-name>`          | `outlook-event-sso-sample` |
+| `<fully-qualified-domain-name>` | `localhost:3000` |
+| Microsoft Graph permissions | profile, openid, user.read |
+
+Follow the steps in [Register an Office Add-in that uses single sign-on (SSO) with the Microsoft identity platform](https://learn.microsoft.com/office/dev/add-ins/develop/register-sso-add-in-aad-v2).
+
+> Note: The instructions have you create a redirect URI for a single-page application. This step isn't necessary for this sample because it doesn't use a fallback authentication approach if SSO fails.
+
+## Configure the Sample
+
+Before you run the sample, you'll need to do a few things to make it work properly.
+
+- In Visual Studio Code (or editor of your choice) , open the root folder for this sample.
+
+### Update manifest.xml
+
+1. Open the **manifest.xml** file.
+1. Find the `<WebApplicationInfo>` section near the bottom of the manifest. Then replace the `Enter_client_ID_here` value, in both places where it appears, with the application ID you generated as part of the app registration process.
+
+>Note: Make sure that the port number in the `Resource` element matches the port used by your project. It should also match the port you used when registering the application.
+
+### Update .ENV
+
+1. Open the **.ENV** file.
+1. Replace the `Enter_client_ID_here` placeholder value with the application ID you generated as part of the app registration process.
+1. Replace the `Enter_client_secret_here` placeholder value with the client secret you generated as part of the app registration process.
+
+## Provide user consent to the app
+
+If you want to try the add-in using a different tenant than the one where you registered the app, you need to do this step.
+
+You have two choices for providing consent:
+
+- All users. Use an administrator account and consent once for all users in your Office 365 tenant
+- Single user. Use any account to consent for just that user
+
+### Provide admin consent for all users
+
+If you have access to a tenant administrator account, this method allows you to provide consent for all users in your organization, which can be convenient if you have multiple developers that need to develop and test your add-in.
+
+1. Browse to `https://login.microsoftonline.com/common/adminconsent?client_id={application_ID}&state=12345`, where `{application_ID}` is the application ID shown in your app registration.
+1. Sign in with your administrator account.
+1. Review the permissions and click **Accept**.
+
+The browser will attempt to redirect back to your app, which may not be running. You might see a "this site cannot be reached" error after clicking **Accept**. This is OK, the consent was still recorded.
+
+### Provide consent for a single user
+
+If you don't have access to a tenant administrator account, or you just want to limit consent to a few users, this method allows you to provide consent for a single user.
+
+1. Browse to `https://login.microsoftonline.com/common/oauth2/authorize?client_id={application_ID}&state=12345&response_type=code`, where `{application_ID}` is the application ID shown in your app registration.
+1. Sign in with your account.
+1. Review the permissions and click **Accept**.
+
+The browser will attempt to redirect back to your app, which may not be running. You might see a "this site cannot be reached" error after clicking **Accept**. This is OK, the consent was still recorded.
+
+## Run the Sample
+
+1. Open a terminal window and run the command `npm install` to install all package dependencies.
+1. Run the command `npm start` to start the web server.
+1. You need to sideload the add-in into Outlook to test it. Follow the instructions at [Sideload an Office Add-in for Testing](https://learn.microsoft.com/office/dev/add-ins/outlook/sideload-outlook-add-ins-for-testing) to sideload the sample.
+1. Compose a new email. The email will display a notification that it will append a signature.
+1. Send the email to yourself. Check when it arrives that the signature is appended.
+
+## SSO and fallback
+
+It's recommended to always have a fallback authentication approach if SSO fails for any reason. However, fallback authentication requires a popup dialog for the user to sign in. It's not possible to open a dialog from an event in Outlook so this sample doesn't use fallback authentication. If an error occurs, the sample displays the error as a notification on the message, and the signature is not appended.
+
+## Questions and feedback
+
+- Did you experience any problems with the sample? [Create an issue](https://github.com/OfficeDev/Office-Add-in-samples/issues/new/choose) and we'll help you out.
+- We'd love to get your feedback about this sample. Go to our [Office samples survey](https://aka.ms/OfficeSamplesSurvey) to give feedback and suggest improvements.
+- For general questions about developing Office Add-ins, go to [Microsoft Q&A](https://learn.microsoft.com/answers/topics/office-js-dev.html) using the office-js-dev tag.
+
+## Copyright
+
+Copyright (c) 2023 Microsoft Corporation. All rights reserved.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information, see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+<img src="https://telemetry.sharepointpnp.com/pnp-officeaddins/samples/outlook-add-in-sso-event-based-activation" />
---
+++ public/commands/commands.js
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+Office.onReady();
+
+// Default SSO settings for acquiring access tokens.
+const defaultSSO = {
+    allowSignInPrompt: true,
+    allowConsentPrompt: true,
+    forMSGraphAccess: true,
+};
+
+/**
+ * Handle the OnNewMessageCompose or OnNewAppointmentOrganizerevent by calling getUserProfile which will
+ * append a signature with the user's profile to the message body.
+ *
+ * @param {Office.AddinCommands.Event} event The OnNewMessageCompose or OnNewAppointmentOrganizer event object.
+ */
+async function onItemComposeHandler(event) {
+    await getUserProfile();
+    event.completed({ allowEvent: true });
+}
+
+Office.actions.associate('onMessageComposeHandler', onItemComposeHandler);
+
+/**
+ * Call the web server API to get the user's free/busy schedule.
+ * The web server will use OBO and call Microsoft Graph to get and return the schedule.
+ */
+async function getUserProfile() {
+    try {
+        // Get access token from Outlook host via SSO.
+        let accessToken = await Office.auth.getAccessToken(defaultSSO);
+        debugger;
+
+        // Call web server which will make Graph call and return filename list.
+        let jsonReponse = await callWebServerAPI(
+            'GET',
+            '/getUserProfile',
+            accessToken
+        );
+
+        // Create signature from user profile.
+        const signature = `${jsonReponse.displayName} \n ${jsonReponse.mail} \n ${jsonReponse.jobTitle} \n ${jsonReponse.mobilePhone}`;
+        await appendTextOnSend(signature);
+    } catch (exception) {
+        // Exceptions are displayed in the notification bar.
+        if (exception.code) {
+            handleClientSideErrors(exception);
+            return;
+        } else {
+            showMessage(exception.message);
+            return;
+        }
+    }
+}
+
+/**
+ * Calls a REST API on the server.
+ * @param {*} method HTTP method to use such as GET, POST, etc...
+ * @param {*} url URL of the REST API.
+ * @param {*} retryRequest Indicates if this is a retry of the call.
+ * @returns The JSON response from the REST API.
+ */
+async function callWebServerAPI(method, url, retryRequest = false) {
+    // Get the access token from Office host using SSO.
+    // Note that Office.auth.getAccessToken modifies the options parameter. Create a copy of the object
+    // to avoid modifying the original object.
+    const options = JSON.parse(JSON.stringify(defaultSSO));
+    const accessToken = await Office.auth.getAccessToken(options);
+
+    const response = await fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken,
+        },
+    });
+
+    // Check for success condition: HTTP status code 2xx.
+    if (response.ok) {
+        return response.json();
+    }
+
+    // Check for fail condition: Is SSO token expired? If so, retry the call which will get a refreshed token.
+    const jsonBody = await response.json();
+    if (
+        jsonBody !== null &&
+        jsonBody.type === 'TokenExpiredError' &&
+        !retryRequest
+    ) {
+        return callWebServerAPI(method, path, true); // Try the call again. The underlying call to Office JS getAccessToken will refresh the token.
+    }
+
+    // Check for fail condition: Did we get a Microsoft Graph API error, which is returned as bad request (403)?
+    if (response.status === 403 && jsonBody.type === 'Microsoft Graph') {
+        throw new Error('Microsoft Graph error: ' + jsonBody.errorDetails);
+    }
+
+    // Handle other errors.
+    throw new Error(JSON.stringify(jsonBody));
+}
+
+/**
+ * Handles any error returned from getAccessToken. The numbered errors are typically user actions
+ * that don't require fallback auth. The text shown for each error indicates next steps
+ * you should take.
+ * @param {*} err The error to process.
+ */
+function handleSSOErrors(err) {
+    switch (err.code) {
+        case 13001:
+            // No one is signed into Office. If the add-in cannot be effectively used when no one
+            // is logged into Office, then the first call of getAccessToken should pass the
+            // `allowSignInPrompt: true` option. Since this sample does that, you should not see
+            // this error.
+            showMessage(
+                'No one is signed into Office. Please sign in before sending.'
+            );
+            break;
+        case 13002:
+            // The user aborted the consent prompt. If the add-in cannot be effectively used when consent
+            // has not been granted, then the first call of getAccessToken should pass the `allowConsentPrompt: true` option.
+            showMessage(
+                'You have not granted consent. If you want to grant consent, try sending again.'
+            );
+            break;
+        case 13006:
+            // Only seen in Office on the web.
+            showMessage(
+                'Office on the web is experiencing a problem. Please sign out of Office, close the browser, and then start again.'
+            );
+            break;
+        case 13008:
+            // Only seen in Office on the web.
+            showMessage(
+                'Office is still working on the last operation. When it completes, try this operation again.'
+            );
+            break;
+        case 13010:
+            // Only seen in Office on the web.
+            showMessage(
+                "Follow the instructions to change your browser's zone configuration."
+            );
+            break;
+        default:
+            // For all other errors, including 13000, 13003, 13005, 13007, 13012, and 50001, show error message.
+            showMessage('Could not sign in: ' + err.code);
+            break;
+    }
+}
+
+/**
+ * Creates information bar to display a message to the user.
+ */
+function showMessage(text) {
+    console.log(text);
+    const id = 'dac64749-cb7308b6d444';
+    const details = {
+        type: Office.MailboxEnums.ItemNotificationMessageType.ErrorMessage,
+        message: text.substring(0,150),
+    };
+    Office.context.mailbox.item.notificationMessages.addAsync(id, details);
+}
+
+/**
+ * Appends text to the end of the message or appointment's body once it's sent.
+ * @param {*} text The text to append.
+ */
+function appendTextOnSend(text) {
+    // It's recommended to call getTypeAsync and pass its returned value to the options.coercionType parameter of the appendOnSendAsync call.
+    Office.context.mailbox.item.body.getTypeAsync((asyncResult) => {
+        if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+            console.log(
+                'Action failed with error: ' + asyncResult.error.message
+            );
+            return;
+        }
+
+        const bodyFormat = asyncResult.value;
+        Office.context.mailbox.item.body.appendOnSendAsync(
+            text,
+            { coercionType: bodyFormat },
+            (asyncResult) => {
+                if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                    console.log(
+                        'Action failed with error: ' + asyncResult.error.message
+                    );
+                    return;
+                }
+
+                showMessage(
+                    `"${text}" will be appended to the body once the message or appointment is sent. Send the mail item to test this feature.`
+                );
+            }
+        );
+    });
+}
